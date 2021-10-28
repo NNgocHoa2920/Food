46f7280ca62fd740889fc09bdad5df1ea03fc43f
NNgocHoa2920 <87591053+NNgocHoa2920@users.noreply.github.com>
2021-10-28 12:05:36 +0700

    first commit

---
+++ app/src/main/java/gts/trainningcourse/mockproject/model/food/Food.kt
@@ -0,0 +1,11 @@
+package gts.trainningcourse.mockproject.model.food
+
+import java.io.Serializable
+
+class Food(
+    override val title: String,
+    override val pic: Int,
+    override val decription: String,
+    override val fee: String,
+    override val category: String,
+) : IFood, Serializable
